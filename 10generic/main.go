1732091af6753d9969c875ebccdec167e1d4ace2
quarks_core <erhanburhanudin@gmail.com>
2024-11-26 20:52:24 +0700

    update biling console

--- 10generic/main.go
+++ 10generic/main.go
@@ -27,27 +27,27 @@ var listProduct []struct {
 		id    int
 		name  string
 		price int
-	}{id: 0, name: "Capucino", price: 9000},
+	}{id: 1, name: "Capucino", price: 9000},
 	struct {
 		id    int
 		name  string
 		price int
-	}{id: 1, name: "Americano", price: 9000},
+	}{id: 2, name: "Americano", price: 9000},
 	struct {
 		id    int
 		name  string
 		price int
-	}{id: 2, name: "Cafelate", price: 9000},
+	}{id: 3, name: "Cafelate", price: 9000},
 	struct {
 		id    int
 		name  string
 		price int
-	}{id: 3, name: "Lemon Tea", price: 9000},
+	}{id: 4, name: "Lemon Tea", price: 9000},
 	struct {
 		id    int
 		name  string
 		price int
-	}{id: 4, name: "Coconut Lemon", price: 9000},
+	}{id: 5, name: "Coconut Lemon", price: 9000},
 }
 
 type itemBuy struct {
@@ -93,7 +93,9 @@ func genericInput[T int32 | string](inpStr *bufio.Reader, retVal T) T {
 		retVal = ff.(T)
 		// map[bool](interface{}){true: retVal, false: panic("Error COnversion")}[err == nil]
 	case string:
-		ff = string("sada")
+		val := strings.Replace(stringInput, "\n", "", -1)
+		ff = string(val)
+		fmt.Println("Debug", ff)
 		retVal = ff.(T)
 	}
 	return retVal
@@ -131,14 +133,18 @@ func billFormater(list []itemBuy) string {
 	fmt.Printf("%-20s:%19s\n", "Customer Name", "Ucup87899")
 	fmt.Printf("%-20s:%19s\n", "Order Date ", "2024-11-11")
 	fmt.Printf("%-20s:%19s\n", "Items ", "")
+	var total int32
 	for k, v := range list {
-		fmt.Println(k, v)
+		fmt.Println(k+1, v)
+		total += int32(v.amount)
 	}
-	fmt.Printf("Amount: %6d| PPN: %6d|\n", 12, 345)
+
+	fmt.Printf("Amount: %6d| PPN: %6d|\n", total, 345)
 	fmt.Println(limiter)
 	return ""
 }
 
+// example this form is allow in golang
 func diamondOperator[T int32 | string](f func(s string, t T) T) {
 
 }
@@ -146,23 +152,29 @@ func diamondOperator[T int32 | string](f func(s string, t T) T) {
 func main() {
 
 	showListmenu()
-	fmt.Println("Please input number menu you want to buy")
 
 	var sInput string
+
 	var iInput int32
-	genericInput(inputStream, iInput)
-	genericInput(inputStream, sInput)
+	var listItemBuy []itemBuy = make([]itemBuy, 0)
 
-	inputConsole()
+	for {
+		fmt.Println("Please input number menu you want to buy")
+		iInput := genericInput(inputStream, iInput)
+		product := findRepo(int(iInput))
+		listItemBuy = append(listItemBuy, itemBuy{
+			id:       product.id,
+			itemName: product.name,
+			qty:      int(iInput),
+			amount:   int(iInput) * product.price,
+		})
 
-	var list1 []itemBuy = []itemBuy{
-		itemBuy{
-			id:       1,
-			itemName: "cok",
-			qty:      90,
-			amount:   9000,
-		},
+		fmt.Println(">do you want buy more")
+		sInput = genericInput(inputStream, sInput)
+		if strings.ToUpper(strings.Trim(sInput, " ")) != "Y" {
+			billFormater(listItemBuy)
+			break
+		}
 	}
-	billFormater(list1)
 
 }
