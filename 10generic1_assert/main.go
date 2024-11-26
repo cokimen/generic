15a6487ab722d96699a873b139b8f1283808df5a
quarks_core <erhanburhanudin@gmail.com>
2024-11-26 22:36:11 +0700

    add more example type assertion

--- 10generic1_assert/main.go
+++ 10generic1_assert/main.go
@@ -2,7 +2,24 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
+
+type Employee struct {
+	firstname string
+}
+
+func (emp Employee) getFirstname() string {
+	return emp.firstname
+}
+
+type Customer struct {
+	custId int
+}
+
+func (c Customer) getCustId() int {
+	return c.custId
+}
 
 func handler[T string | int32 | interface{}](data T) T {
 	// var retVal T
@@ -16,6 +33,24 @@ func handler[T string | int32 | interface{}](data T) T {
 		fmt.Println("Data Type is interface")
 	}
 	return data
+}
+
+func serializer(a interface{}) {
+	fmt.Println("serializer part")
+	switch a.(type) {
+	case string:
+		fmt.Println("string")
+	case int32:
+		fmt.Println("int32")
+	case byte:
+		fmt.Println("byte")
+	case float32:
+		fmt.Println("float32")
+	case struct{}:
+		fmt.Println("struct")
+	default:
+		fmt.Println("didnt defined")
+	}
 }
 
 func main() {
@@ -38,4 +73,35 @@ func main() {
 	var d []string = handler([]string{"erhan", "burhanudin"})
 
 	fmt.Println(d)
+
+	fmt.Println("-----------------------------------------------")
+
+	var emp1 Employee = Employee{firstname: "jaeger"}
+
+	var cust1 Customer = Customer{custId: int(90)}
+
+	var container []interface{} = []interface{}{emp1, cust1}
+
+	for k, v := range container {
+		fmt.Println("Index ", k)
+		switch v.(type) {
+		case Employee:
+			fmt.Println(v.(Employee).getFirstname())
+			fmt.Println(reflect.TypeOf(v))
+			fmt.Println(">>>>")
+		case Customer:
+			fmt.Println(v.(Customer).getCustId())
+			fmt.Println(reflect.TypeOf(v))
+			fmt.Println(">>>>")
+		default:
+			fmt.Println("Unknown")
+		}
+	}
+
+	fmt.Println("---------------------------------------")
+
+	serializer('a')
+	serializer("Cokimen")
+	serializer(int32(900))
+	serializer(float32(90.99))
 }
